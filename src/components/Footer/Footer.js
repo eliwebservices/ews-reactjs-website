b43c12170c29aa4f23bbb65c6bff9afbdb9f2e7c
unknown <herbertkwablat@gmail.com>
2023-04-14 07:04:04 -0700

    footer design completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,73 +1,86 @@
 import React from "react";
 import "./Footer.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faFacebook,
+	faInstagram,
+	faTwitter,
+	faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
 	return (
 		<div>
 			<section className="footer_section">
 				<div className="divider_before"></div>
-				<div className="footer_content row">
-					<div className="footer_logo col-lg-4 col-md-6">
-						<h5>Follow us</h5>
-						<div className="social_handles">
-							<a href="https://web.facebook.com/eliwebservices">
-								<i className="fab fa-facebook"></i>
-							</a>
-							<a href="https://instagram.com/eliwebservices">
-								<i className="fab fa-instagram"></i>
-							</a>
-							<a href="https://twitter.com/eliwebservices">
-								<i className="fab fa-twitter"></i>
-							</a>
-							<a href="https://www.linkedin.com/company/eliwebservices">
-								<i className="fab fa-linkedin-in"></i>
-							</a>
-						</div>
+				<Container className="footer_bt_cn">
+					<Row className="footer_content">
+						<Col lg={4} md={6} className="footer_logo">
+							<h5>Follow us</h5>
+							<div className="social_handles">
+								<a href="https://web.facebook.com/eliwebservices">
+									<FontAwesomeIcon icon={faFacebook} />
+								</a>
+								<a href="https://instagram.com/eliwebservices">
+									<FontAwesomeIcon icon={faInstagram} />
+								</a>
+								<a href="https://twitter.com/eliwebservices">
+									<FontAwesomeIcon icon={faTwitter} />
+								</a>
+								<a href="https://www.linkedin.com/company/eliwebservices">
+									<FontAwesomeIcon icon={faLinkedinIn} />
+								</a>
+							</div>
 
-						{/* <!-- <img src="../img/footer_logo_.png" alt="" className="footer_logo_img" /> --> */}
-					</div>
+							{/* <!-- <img src="../img/footer_logo_.png" alt="" className="footer_logo_img" /> --> */}
+						</Col>
 
-					<div className="contact_info col-lg-4 col-md-6">
-						<div className="footer_contact_container">
-							<h5>Contact us</h5>
-							<p>info@eliwebdev.com</p>
-							<p>P.O BOX MS 102 MILE7, ACCRA-GHANA</p>
-							<p>+233(0)201599071</p>
-						</div>
-					</div>
+						<Col lg={4} md={6} className="contact_info">
+							<div className="footer_contact_container">
+								<h5>Contact us</h5>
+								<p>info@eliwebdev.com</p>
+								<p>P.O BOX MS 102 MILE7, ACCRA-GHANA</p>
+								<p>+233(0)201599071</p>
+							</div>
+						</Col>
 
-					<div className="footer_form col-lg-4 col-md-6">
-						<h5>Subscribe to our newsletter</h5>
-						<form action="/" method="post">
-							<input
-								type="email"
-								name="email"
-								id=""
-								className="form-control"
-								placeholder="E-mail"
-								required
-								pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-							/>
-							<button
-								type="submit"
-								className="btn"
-								id="subscribe_btn"
-								name="subscribe_btn"
-							>
-								Subscribe
-							</button>
-						</form>
-					</div>
+						<Col lg={4} md={6} className="footer_form">
+							<h5>Subscribe to our newsletter</h5>
+							<form action="/" method="post">
+								<input
+									type="email"
+									name="email"
+									id=""
+									className="form-control"
+									placeholder="E-mail"
+									required
+									pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+								/>
+								<button
+									type="submit"
+									className="btn"
+									id="subscribe_btn"
+									name="subscribe_btn"
+								>
+									Subscribe
+								</button>
+							</form>
+						</Col>
 
-					<div className="logo-slogan">
-						<img src="../img/footer_logo_.png" alt="" />
-						<p className="footer-slogan">...building Visions Together</p>
-					</div>
+						<Col lg={12} md={12} sm={12} className="logo-slogan">
+							<img src="../img/footer_logo_.png" alt="" />
+							<p className="footer-slogan">...building Visions Together</p>
+						</Col>
 
-					{/* <!-- <div className="contact_info">
+						{/* <!-- <div className="contact_info">
             <p>+233(0)201599071 | info@eliwebdev.com</p>
         </div> --> */}
-				</div>
+					</Row>
+				</Container>
 			</section>
 		</div>
 	);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import Footer from "../components/Footer/Footer";
 const Home = () => {
 	return (
 		<motion.div
-			className="container text-center  bg-black"
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			exit={{ opacity: 0 }}
