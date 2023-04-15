ce1a6b2d1099b2a5ba938f6247e7dffa116e6dc3
unknown <herbertkwablat@gmail.com>
2023-04-14 22:01:44 -0700

    404 page desktop design completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			exit={{ opacity: 0 }}
-			transition={{ duration: 3 }}
+			transition={{ duration: 0.5 }}
 		>
 			<Navbar toggleBtn={toggleDropdown} />
 			<MobileNavbar
--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Ews404 from "../images/ews-404.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/NoPage.css";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import MobileNavbar from "../components/MoblieNavbar/MobileNavbar";
+import Footer from "../components/Footer/Footer";
 
 const NoPage = () => {
-	return <div>NoPage</div>;
+	const navigate = useNavigate();
+	// console.log(navigate);
+
+	const goBack = () => {
+		navigate(-1);
+	};
+
+	const [open, setOpen] = useState(false);
+
+	function toggleDropdown() {
+		setOpen(!open);
+	}
+
+	function closeDropdown() {
+		setOpen(!open);
+	}
+
+	return (
+		<div>
+			<Navbar toggleBtn={toggleDropdown} />
+			<MobileNavbar
+				mobileTab={{ marginLeft: open ? "0" : "100%" }}
+				closeDropdown={closeDropdown}
+			/>
+			<Container>
+				<Row className="page_404_text">Oops!</Row>
+				<Row>
+					<div className="page-404-image">
+						<img src={Ews404} alt="" />
+					</div>
+				</Row>
+				<Row className="page_404_text">sorry, page not found</Row>
+				<Row className="page_404_btn">
+					<button onClick={goBack}>Go Back</button>
+				</Row>
+			</Container>
+			<Footer />
+		</div>
+	);
 };
 
 export default NoPage;
